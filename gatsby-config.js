70512856b43a702f915a51afefdbc7c045a9d9b9
antjanus <antonin@antjanus.com>
2018-04-25 23:48:11 -0600

    make available offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-feed`,
-//    `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-typography',
